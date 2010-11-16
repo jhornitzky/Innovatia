1cc767561e0974bba0a35bac82cab7463bc51ef4
charlie chan <nexos@nexos-laptop.(none)>
2010-11-16 18:14:10 +1100

    Make dojo load faster by commiting layer file Doesn't seem to work in FF though

---
+++ ui/scripts/dojoLayer.js
@@ -0,0 +1,8 @@
+dojo.require("dijit.Dialog");
+dojo.require("dijit.form.Button");
+dojo.require("dijit.layout.TabContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.Menu");
+dojo.require("dijit.form.ComboBox");
+dojo.require("dijit.form.Textarea");
+dojo.require("dojo.parser");
